d6c780a95fca50705b7245895155903572cfe774
ghais <brunoghais@gmail.com>
2021-02-17 01:25:53 -0300

    Fix:
     - A app nao precisa mais dessa parte do codigo, pois agora funciona com uma tabela

--- client_src/src/components/Main.js
+++ client_src/src/components/Main.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Divida from './Dividas';
-import DividaDetails from './DividaDetails';
 
 const Main = () => (
     <main>
         <Switch>
             <Route exact path='/' component={Divida} />
-            <Route exact path='/dividas/:id' component={DividaDetails} />
         </Switch>
     </main>
 )
